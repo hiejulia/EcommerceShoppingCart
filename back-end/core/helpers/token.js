f4e89cb8275c0bca9db9821835b04472110b4352
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-08 23:02:51 +0300

    create helper for token

--- back-end/core/helpers/token.js
+++ back-end/core/helpers/token.js
@@ -2,4 +2,26 @@
 
 const LEN = 16;
 const crypto = require('crypto');
+//generateToken
+function generateToken(randomBytes, callback) {
+  if (typeof randomBytes === 'function') {
+    callback = randomBytes;
+    randomBytes = LEN;
+  }
 
+  // we will return the token in `hex`
+  randomBytes = randomBytes / 2;
+
+  crypto.randomBytes(randomBytes, (err, buf) => {
+    if (err) {
+      return callback(err);
+    }
+
+    var token = buf.toString('hex');
+
+    callback(null, token);
+  });
+};
+
+
+module.exports.generate = generateToken;
